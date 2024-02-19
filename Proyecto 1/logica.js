473724363108641e4150afb1a96d9b37c3dde0cc
Johan97-crypto <158749037+Johan97-crypto@users.noreply.github.com>
2024-02-19 07:30:04 -0500

    clase de servicios telematicos
    trabajos y apuntes de clase

---
+++ Proyecto 1/logica.js
@@ -0,0 +1,6 @@
+var id1=document.getElementById("item1").innerHTML="uno";
+var id2=document.getElementById("item2").innerHTML="dos";
+var id3=document.getElementById("item3").innerHTML="tres";
+var id4=document.getElementById("item4").innerHTML="cuatro";
+var id5=document.getElementById("item5").innerHTML="cinco";
+/* agregar imagenes */
